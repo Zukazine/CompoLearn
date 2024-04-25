a3f1fc722d0424e0b0cf8e159749b063f08f7450
Zukazine <zukazine007@gmail.com>
2024-04-25 10:08:11 +0700

    hamburger menu

--- src/app/button/page.tsx
+++ src/app/button/page.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import HamburgerButton from "./_components/hamburgerButton";
 
 const ButtonPage = () => {
     return ( 
         <>
-            
+            <HamburgerButton />
             <Link href={'/'} >
                 <div className="flex items-center justify-center w-[5vw] h-[5vh] border-red-500 border-2 hover:bg-red-200 hover:bg-opacity-50">
                     Back
