dd59a97b22fb75eadddb49d4d29fb6f664d84211
Zukazine <zukazine007@gmail.com>
2024-04-25 13:29:34 +0700

    spotlight button done, init neuromorphism button

--- src/app/button/page.tsx
+++ src/app/button/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import HamburgerButton from "./_components/hamburgerButton";
 import EncryptButton from "./_components/encryptButton";
 import DottedButton from "./_components/dottedButton";
+import SpotlightButton from "./_components/spotlightButton";
 
 const ButtonPage = () => {
     return ( 
@@ -9,6 +10,7 @@ const ButtonPage = () => {
             <HamburgerButton />
             <EncryptButton />
             <DottedButton />
+            <SpotlightButton />
 
             <Link href={'/'} >
                 <div className="flex items-center justify-center w-[5vw] h-[5vh] border-red-500 border-2 hover:bg-red-200 hover:bg-opacity-50">
