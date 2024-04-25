32e0b2b46339251fa3cfb0925972e225bbc90a09
Zukazine <zukazine007@gmail.com>
2024-04-25 10:54:46 +0700

    Encrypt Button Done, Init Dotted Button

--- src/app/button/page.tsx
+++ src/app/button/page.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
 import HamburgerButton from "./_components/hamburgerButton";
+import EncryptButton from "./_components/encryptButton";
 
 const ButtonPage = () => {
     return ( 
         <>
             <HamburgerButton />
+            <EncryptButton />
+            
             <Link href={'/'} >
                 <div className="flex items-center justify-center w-[5vw] h-[5vh] border-red-500 border-2 hover:bg-red-200 hover:bg-opacity-50">
                     Back
