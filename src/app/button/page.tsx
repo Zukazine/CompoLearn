2de6fbf2b41c40af711a383fa6d93ad25cc8c80a
Zukazine <zukazine007@gmail.com>
2024-04-25 11:11:11 +0700

    dotted button done, init spotlight button

--- src/app/button/page.tsx
+++ src/app/button/page.tsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
 import HamburgerButton from "./_components/hamburgerButton";
 import EncryptButton from "./_components/encryptButton";
+import DottedButton from "./_components/dottedButton";
 
 const ButtonPage = () => {
     return ( 
         <>
             <HamburgerButton />
             <EncryptButton />
-            
+            <DottedButton />
+
             <Link href={'/'} >
                 <div className="flex items-center justify-center w-[5vw] h-[5vh] border-red-500 border-2 hover:bg-red-200 hover:bg-opacity-50">
                     Back
