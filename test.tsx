03a31427283791563e1c8ff596e00e1581a58395
Zukazine <zukazine007@gmail.com>
2024-04-29 10:28:33 +0700

    cards training done

--- test.tsx
+++ test.tsx
@@ -1,71 +1,65 @@
 import React from "react";
+import { 
+	FiCreditCard, 
+	FiMail, 
+	FiUser, 
+	FiUsers 
+} from "react-icons/fi";
 
-const DrawOutlineButton = () => {
+const Card = ({
+	title,
+	subtitle,
+	Icon, 
+	href
+}) => {
 	return (
-		<div>
-			<AnimatedDrawOutlineButton>Hover me</AnimatedDrawOutlineButton>
-		</div>
-	)
-}
-
-const AnimatedDrawOutlineButton = ({children, ...rest}) => {
-	return (
-		<button
-			{...rest}
+		<a
+			href={href}
 			className="
-			group relative 
-			px-4 py-2 
-			font-medium text-slate-100 
-			
-			transition-colors duration-[400ms] 
-			
-			hover:text-indigo-300" 
+				relative group w-full p-4 overflow-hidden 
+				rounded-border-[1px] border-slate-300 bg-white"
 		>
-			<span>{children}</span>
-
-			{/* TOP */}
-			<span className="
-				absolute left-0 top-0 
-				h-[2px] w-0 bg-indigo-300 
+			<div className="
+				absolute inset-0
+				bg-gradient-to-r from-violet-600 to-indigo-600 
+				translate-y-[100%] group-hover:translate-y-[0%]
 				
-				transition-all duration-100 
-				
-				group-hover:w-full"
+				transition-transform duration-300
+				"
 			/>
+			<Icon className="
+				absolute z-10 -top-12 -right-12 
+				text-9xl text-slate-100 group-hover:text-violet-600
+				group-hover:rotate-12
+
+				transition-transform duration-300
+			"/>
+			<Icon className="
+				relative z-10 mb-2 
+				text-2xl text-violet-600 group-hover:text-white
+
+				transition-colors duration-300
+			"/>
+			<h3 className="
+				relative z-10
+				font-medium text-lg text-slate-950 group-hover:text-white 
+
+				duraiton-300
+			">
+
+				{title}
 			
-			{/* RIGHT */}
-			<span className="
-				absolute right-0 top-0
-				h-0 w-[2px] bg-indigo-300
-				
-				transition-all delay-100 duration-100
+			</h3>
+			<p className="
+				relative z-10 
+				text-slate-400 group-hover:text-violet-200
+			
+				duration-300
+			">
 
-				group-hover:h-full
-				"
-			/>
-
-			{/* BOTTOM */}
-			<span className="
-				absolute bottom-0 left-0
-				h-[2px] w-0 bg-indigo-300
-				
-				transition-all delay-200 duration-100
-
-				group-hover:w-full
-				"
-			/>
-
-			{/* LEFT */}
-			<span className="
-				absolute bottom-0 left-0
-				h-0 w-[2px] bg-indigo-300
-
-				transition-all delay-300 duration-100
-
-				group-hover:h-full
-			"
-			/>
-
-		</button>
+				{subtitle}
+			
+			</p>
+		</a>
 	)
 }
