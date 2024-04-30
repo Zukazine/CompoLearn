59b95700a1d2ff369e1fcf4e947c6f7db02040d1
Zukazine <zukazine007@gmail.com>
2024-04-30 14:20:43 +0700

    basic stuff 1 being harsh

--- src/app/others/_component/counterOne.tsx
+++ src/app/others/_component/counterOne.tsx
@@ -1,11 +1,36 @@
-import { useEffect } from "react";
+'use client'
+
+import { useEffect, useState, useRef } from "react";
+import { IoConstructOutline } from "react-icons/io5";
 
 const CounterOne = () => {
-    useEffect
-    
-    return ( 
+	const [num, setNum] = useState(0)
+	console.log(num)	
+	useInterval(() => {setNum(num + 1)}, 1000)
 
-    );
+	return ( 
+		<div className="grid place-items-center bg-slate-300 h-screen">
+			<div className="text-lg ">
+				{num}
+			</div>
+		</div>
+	);
 }
  
 export default CounterOne;
+
+const useInterval = ( callback,  delay ) => {
+	const savedCallback = useRef()
+	
+	useEffect(() => {
+		savedCallback.current = callback
+	}, [callback])
+
+	useEffect(() => {
+		let id = setInterval(() => {
+			savedCallback.current
+		}, delay)
+
+		return () => clearInterval(id)
+	}, [delay])
+}
--- src/app/others/page.tsx
+++ src/app/others/page.tsx
@@ -1,8 +1,11 @@
 import Link from "next/link";
+import CounterOne from "./_component/counterOne";
 
 const DropwdownPage = () => {
 	return ( 
 		<>
+			<CounterOne />
+
 			<Link href={'/'} >
 				<div className="fixed flex items-center justify-center w-[5vw] h-[5vh] border-red-500 border-2 hover:bg-red-200 hover:bg-opacity-50">
 					Back
