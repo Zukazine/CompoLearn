d5128381a7021e6320c19114073c4c30416b7f10
Zukazine <zukazine007@gmail.com>
2024-04-30 14:31:10 +0700

    Counter One Two Ready

--- src/app/others/_component/counterOne.tsx
+++ src/app/others/_component/counterOne.tsx
@@ -4,14 +4,30 @@ import { useEffect, useState, useRef } from "react";
 import { IoConstructOutline } from "react-icons/io5";
 
 const CounterOne = () => {
-	const [num, setNum] = useState(0)
+	const [num, setNum] = useState(100000)
 	console.log(num)	
-	useInterval(() => {setNum(num + 1)}, 1000)
+	// useInterval(() => {setNum(num + 1)}, 1000)
 
-	return ( 
+ 	return ( 
 		<div className="grid place-items-center bg-slate-300 h-screen">
-			<div className="text-lg ">
+			<div className="flex flex-col gap-4 justify-center items-center bg-yellow-300 p-8 rounded-3xl">
+			<div className="text-6xl font-mono font-medium">
 				{num}
+			</div>
+			<div className="flex gap-4">
+				<button 
+					className="flex justify-center items-center text-2xl bg-green-500 rounded-lg px-4 font-bold text-white transition-transform duration-300"
+					onClick={() => {setNum(num + 1)}}
+				>
+					+
+				</button>
+				<button
+					className="flex justify-center items-center text-2xl bg-red-500 rounded-lg px-4 font-bold text-white transition-transform duration-300" 
+					onClick={() => {setNum(num - 1)}}
+				>
+					-
+				</button>
+			</div>
 			</div>
 		</div>
 	);
